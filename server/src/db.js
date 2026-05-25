904f0da32d8e70e467444f507289328043187a4c
SJYssr <SJYssr@users.noreply.github.com>
2026-05-25 19:24:20 +0800

    fix: MySQL dateStrings=true直接返回CST字符串，不再前端转时间

--- server/src/db.js
+++ server/src/db.js
@@ -10,7 +10,8 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  charset: 'utf8mb4'
+  charset: 'utf8mb4',
+  dateStrings: true
 });
 
 export default pool;
